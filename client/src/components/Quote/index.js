2e483697a5a0882e6a6984379f144a4387cd81b9
Facundo2210 <74992465+Facundo2210@users.noreply.github.com>
2021-08-01 15:58:14 -0300

    lcsm

--- client/src/components/Quote/index.js
+++ client/src/components/Quote/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
-import {StyledContainer} from './styled';
+import {StyledContainer} from './styled.js';
 
 const Quote = () => {
 	const [Result, SetResult] = useState([]);
