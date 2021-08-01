449085c5ec412743dfe5ac1db4f3cfbc46b7202f
Facundo2210 <74992465+Facundo2210@users.noreply.github.com>
2021-08-01 15:47:43 -0300

    errrror

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,12 @@ import storage from 'local-storage-fallback';
 import {ThemeProvider} from 'styled-components';
 import {lightTheme, darkTheme} from './utils/Themes';
 import {getInitialTheme} from './controllers';
-import About from './Components/About';
-import Skills from './Components/Skills';
-import Projects from './Components/Projects';
-import Contact from './Components/Contact';
-import Footer from './Components/Footer';
-import Nav from './Components/Nav';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
 
 function App() {
 	const [theme, setTheme] = useState(getInitialTheme);
